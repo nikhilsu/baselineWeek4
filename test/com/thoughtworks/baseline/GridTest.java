47c46060c482c0ca25e24ee5fd426856189c0ad4
nikhilsu <nikhilsu@thoughtworks.com>
2015-09-19 12:40:11 +0530

    Added Spec for the Grid should return no cell when the probed column and row indices are greater than the size of the 2D array

--- test/com/thoughtworks/baseline/GridTest.java
+++ test/com/thoughtworks/baseline/GridTest.java
@@ -61,4 +61,12 @@ public class GridTest {
 
         assertEquals(' ', grid.cellAt(-1, -1));
     }
+
+    @Test
+    public void shouldReturnNoCellWhenTheProbedColumnAndRowIndicesAreGreaterThanTheLengthOfThe2DArray() {
+        char[][] cells = {{'X', '-'}, {'X', 'X'}};
+        Grid grid = new Grid(cells);
+
+        assertEquals(' ', grid.cellAt(2, 2));
+    }
 }
